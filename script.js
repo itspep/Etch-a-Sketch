1136f94492029d4e7215ad4f15facd24181cf2ee
Mumuni Tahiru <peptahiru@gmail.com>
2023-02-18 21:43:22 +0000

    commit

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let container=document.getElementById("bigContainer");
-    const cellCount = 16 * 16;
-    for (let i = 0; i < cellCount; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        container.appendChild(cell);
-    }
+const container=document.getElementById("container");
+for(let i=0; i<256; i++){
+    const square=document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square); 
+}
+const events=document.querySelectorAll(".square");
+for(let i=0; i<events.length; i++){
+    events[1].addEventListener("mouseover", function(){
+        events[1].styles.backgroundColor="black";
+    })
+}
